2c3c57ded306a9fdd22be3aeb45070efc578ebc6
Samuel-Ayala <a20166262@pucp.edu.pe>
2023-02-06 12:32:23 -0500

    fourth

---
+++ Ejercicios/4_funciones.py
@@ -0,0 +1,16 @@
+#----Ejercicio 1----#
+#Escribir un programa que reciba un numero como argumento de entrada y diga si el
+#numero es primo o no
+
+
+
+
+#----Ejercicio 2----#
+#Escribir una función que reciba un número entero positivo y devuelva su factorial.
+
+
+
+
+#----Ejercicio 3----#
+#Escribir una función que reciba como argumento de entrada un numero en base 10 y retorne el mismo
+#numero, pero en binario
---
+++ Ejercicios/5_arreglos_y_diccionarios.py
@@ -0,0 +1,17 @@
+#----Ejercicio 1----#
+#Escribir una función que reciba una muestra de números en una lista y devuelva su media.
+
+
+
+#----Ejercicio 2----#
+#Escribir una función que reciba una muestra de números en una lista y devuelva otra lista 
+#con sus cuadrados.
+
+
+
+#----Ejercicio 3----#
+#Escribir una función que reciba una muestra de números en una lista y devuelva un diccionario con su
+#media, varianza y desviación típica.
+
+
+
