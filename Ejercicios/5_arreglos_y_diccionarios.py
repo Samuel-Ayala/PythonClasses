a279fbab1f8a62949260cf412d0b8af0880cf516
Samuel-Ayala <a20166262@pucp.edu.pe>
2023-02-07 10:43:29 -0500

    update2

--- Ejercicios/5_arreglos_y_diccionarios.py
+++ Ejercicios/5_arreglos_y_diccionarios.py
@@ -11,7 +11,54 @@
 
 #----Ejercicio 3----#
 #Escribir una función que reciba una muestra de números en una lista y devuelva un diccionario con su
-#media, varianza y desviación típica.
+#media, mediana y moda.
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# array = [1,3,4,2,4,2,4,3,6]
+# dic_moda = {}
+# for i in array:
+#     key = str(i)
+#     if key in dic_moda.keys():
+#         dic_moda[key] += 1
+#     else:
+#         dic_moda[key] = 1
+
+# len_moda = 0
+# moda = 0
+# for i in dic_moda.keys():
+#     if dic_moda[i] > len_moda:
+#         len_moda = dic_moda[i]
+#         moda = int(i)
+
+        
+# print(moda)
+# print(dic_moda)
+# print(dic_moda.keys())
---
+++ Ejercicios/6_modulos_python_1.py
@@ -0,0 +1,24 @@
+#----Ejercicio 1----#
+#Crear un arreglo de 10 unos con Numpy
+
+
+
+
+#----Ejercicio 2----#
+#Crear un arreglo con 20 numeros aleatorios entre 10 y 30. Luego, hallar la media, mediana, el valor
+#maximo y el valor minimo. Graficar los resultados en un grafico de barra con su respectiva leyenda.
+
+
+
+
+#----Ejercicio 3----#
+#Crear un arreglo con 20 numeros aleatorios entre 1 y 90. Luego, imprimir todos aquellos numeros impares
+#que son mayores a 5
+
+
+
+
+#----Ejercicio 4----#
+#Crear un arreglo con 100 valores aleatorios entre 1 y 1000. Luego, halle su mediana haciendo uso de
+#métodos tradicionales y luego halle su mediana haciendo uso del modulo statistics. Halle el tiempo
+#de ejecucion para cada caso y grafiquelo en barras.
